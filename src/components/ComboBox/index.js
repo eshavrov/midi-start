49c993ba8bfe15a7fa2af287ada8851a1329f128
eshavrov <shavrov.ep@gmail.com>
2020-08-18 00:22:14 +0300

    Fixed ComboBox

--- src/components/ComboBox/index.js
+++ src/components/ComboBox/index.js
@@ -3,15 +3,19 @@ import React from "react";
 const ComboBox = (props) => {
   const { options, value, onChange } = props;
 
-  const _onChange = React.useCallback((event) => {
-    if (!onChange) {
-      return;
-    }
-    onChange(event.target.value);
-  }, [onChange]);
+  const _onChange = React.useCallback(
+    (event) => {
+      if (!onChange) {
+        return;
+      }
+
+      onChange(event.target.value);
+    },
+    [onChange]
+  );
 
   return (
-    <select className="select-css" defaultValue={value} onChange={_onChange}>
+    <select className="select-css" value={value} onChange={_onChange}>
       {options.map(({ title, value: optionValue }, index) => (
         <option key={index} value={optionValue}>
           {title}
