23107163d9ea8e99b0c7a07bb59e5710113ca627
eshavrov <shavrov.ep@gmail.com>
2020-08-23 18:53:13 +0300

    Removed comments

--- src/components/ProgressBar/styles.js
+++ src/components/ProgressBar/styles.js
@@ -9,7 +9,6 @@ const Progress = styled.div`
   border-radius: 25px;
   padding: 10px;
   margin-top: 24px;
-  /* box-shadow: inset 0 -1px 1px rgba(255,255,255,0.3); */
   width: 90%;
 `;
 
@@ -21,14 +20,6 @@ const Value = styled.span`
   border-top-left-radius: 12px;
   border-bottom-left-radius: 12px;
   background-color: rgb(43, 194, 83);
-  /* background-image: linear-gradient(
-    center bottom,
-    rgb(43,194,83) 37%,
-    rgb(84,240,84) 69%
-  );
-  box-shadow: 
-    inset 0 2px 9px  rgba(255,255,255,0.3),
-    inset 0 -2px 6px rgba(0,0,0,0.4); */
   position: relative;
   overflow: hidden;
 `;
